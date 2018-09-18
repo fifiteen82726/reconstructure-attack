3ec2df47592f8a935a488d58a96bb16a6c25d71b
fifiten82726 <fifiteen82726@gmail.com>
2018-09-17 23:18:47 -0400

    finish and add gitignore

--- hw1.py
+++ hw1.py
@@ -2,28 +2,85 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import math
+import random
+from itertools import combinations
 
-# Programming exercise.
+class Reconstruction:
+  def __init__(self, n, e):
+    self.real_dataset = self.create_data_set(n)
+    self.e = int(e)
+    self.n = n
+    self.all_combination = []
+    self.attacked_comination_hash_table = {}
 
-# The goal of this exercise is writing a program implementing the (exponential time) reconstruction attack we saw in class which you can find described in Chapter 8 of the book DR linked from the course webpage - you can also look at the original paper linked here:
-# http://www.wisdom.weizmann.ac.il/%7Edinuri/mypapers/db_privacy.pdf
-# The goal of the exercise is to define a function "attack" which takes in input a dataset D consisting of n records with 1 binary attribute and generate a database D’ which is at hamming distance less than 4E from D. The function attack can access the database D only using queries that are protected via uniform noise with perturbation E=sqrt(n).
+    for r in range(1, n):
+      self.all_combination.append(self.rSubset(range(1, n + 1), r))
 
-# Here some steps I suggest you to follow:
-# 1 - Define a function which allows you to create datasets consisting of n records with 1 binary attribute - you can use this function to create the database D and also to create candidate datasets during the attack.
-# 2 - Define a function implementing sum queries. It may be convenient to have the function running multiple sum queries and returning the results for all of them.
-# 3 - Define a function implementing noisy sum queries: sum queries whose result is perturbed using uniform noise in the interval [0,E]. Also in this case it may be convenient to have the function running multiple noisy sum queries and returning the results for all of them
-# 4 - Define a function attack(D) which takes in input a dataset D and generate a database D’ following the three phases of the reconstruction attack presented in class: query phase, rule out phase, output phase. In the query phase and in the rule out phase you may want to use the functions that you have defined at the previous steps.
-# 5 - Test your function attack by defining a function which checks that the dataset that the function attack outputs is at most at hamming distance 4E from D (by printing the hamming distance on the screen or by any other method that I can also check). I suggest that you repeat this test on several datasets D. To make it feasible I suggest you to use very small values of n, for example n=10 and n=12.
+    print "Real Dataset:          " + str(self.real_dataset)
 
-# return datasets consisting of n records with 1 binary attribute
-def create_data_set(n):
-  return np.random.choice([0, 1], size=(n))
+  # recursive to return all combination
+  def rSubset(self, arr, r):
+    return list(combinations(arr, r))
 
-def sum_query(dataset):
-  return sum(dataset)
+  def create_data_set(self, n):
+    return list(np.random.choice([0, 1], size=(n)))
 
+  def sum_queries(self, query_index, dataset = None):
+    sum_result = 0
+    target_dataset = dataset or self.real_dataset
 
-dataset = create_data_set(10)
-print dataset
-print sum_query(dataset)
+    for i in query_index:
+      sum_result += target_dataset[i - 1]
+    return sum_result
+
+  def noise_sum_queries(self, query_index):
+    num = self.sum_queries(query_index)
+    # add some noise (between 0 to e)
+    random_noise = random.choice(range(int(self.e) + 1))
+    return num + random_noise
+
+  def attack(self):
+    # loop to get noise_sum_queries
+    guess_dataset = [0] * self.n
+    for combine in self.all_combination:
+      for el in combine:
+        noise_sum_queries_num = self.noise_sum_queries(el)
+
+        # these number must be one
+        if (len(el) == 1 and noise_sum_queries_num > self.e):
+          guess_dataset[el[0] - 1] = 1
+        else:
+          if(noise_sum_queries_num - len(el) == self.e):
+            for i in el:
+              guess_dataset[i - 1] = 1
+
+      if(self.authenticate(guess_dataset)):
+        print "Reconstrcture Dataset: " + str(guess_dataset)
+        return True
+
+    print "Fail Attack!"
+    return False
+
+  def authenticate(self, attacked_dataset):
+    a = 0
+    for combine in self.all_combination:
+      for el in combine:
+        query_sum = self.sum_queries(el)
+        attack_sum = self.sum_queries(el, attacked_dataset)
+        if(abs(query_sum - attack_sum) > e):
+          return False # Rule out
+    return True
+
+n = 10
+e = math.sqrt(n)
+
+print "n = " + str(n)
+print "E = " + str(e)
+
+for i in range(10):
+  print "======================= Test " + str(i+1) + " ============================"
+  reconstruction1 = Reconstruction(n, e)
+  reconstruction1.attack()
+  print "\n"
+
